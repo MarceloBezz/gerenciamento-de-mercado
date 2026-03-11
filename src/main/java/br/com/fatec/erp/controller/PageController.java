1af9217b0fbb4be359a434f84652e682332d6a70
Marcelo <mdbezerra16@gmail.com>
2026-03-11 10:00:30 -0300

    feat: :sparkles: Mensagens de erro e logout na tela de login
    Implementação do bootstrap na tela de login

--- src/main/java/br/com/fatec/erp/controller/PageController.java
+++ src/main/java/br/com/fatec/erp/controller/PageController.java
@@ -1,14 +1,21 @@
 package br.com.fatec.erp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
-    
-    @GetMapping("/login")
-    public String login() {
+
+    @GetMapping({"/login", "/"})
+    public String login(@RequestParam(required = false) String logout,
+            @RequestParam(required = false) String error, Model model) {
+        if (logout != null)
+            model.addAttribute("logout", logout);
+        if (error != null)
+            model.addAttribute("error", error);
         return "login";
     }
-    
+
 }
