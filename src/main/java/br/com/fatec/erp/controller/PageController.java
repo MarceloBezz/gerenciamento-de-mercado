e525aacb1ce71f0cf30ccd0d450859c8ae7f001b
Marcelo <mdbezerra16@gmail.com>
2026-03-10 23:00:16 -0300

    feat: :sparkles: Tela de login
    Controller de páginas para acessar as páginas HTML

---
+++ src/main/java/br/com/fatec/erp/controller/PageController.java
@@ -0,0 +1,14 @@
+package br.com.fatec.erp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    
+}
