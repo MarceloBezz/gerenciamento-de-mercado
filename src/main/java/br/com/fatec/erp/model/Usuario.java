9d043a6cd778dbd4a9e9e8a5eca30e32d36ae88f
Pedro-Almeida1 <pedro15almeida12@gmail.com>
2026-03-14 11:10:22 -0300

    feat: :sparkles: Adicionando o atributo nome para a classe usuario

--- src/main/java/br/com/fatec/erp/model/Usuario.java
+++ src/main/java/br/com/fatec/erp/model/Usuario.java
@@ -14,6 +14,8 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
+    private String nome;
     @Column(unique = true, nullable = false)
     private String email;
     @Column(nullable= false)
