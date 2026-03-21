3addccff681464f033c56f942da279c02c5181ac
Pedro-Almeida1 <pedro15almeida12@gmail.com>
2026-03-21 11:13:52 -0300

    feat: :sparkles: Cria Exception, implementa métodos em  UsuarioService
    - cria a classe Exception para tratar exceções, método buscarPorId e método buscarPorEmail

---
+++ src/main/java/br/com/fatec/erp/exception/TratadorDeErros.java
@@ -0,0 +1,13 @@
+package br.com.fatec.erp.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class TratadorDeErros {
+
+//    @ExceptionHandler(Exception.class)
+//    public String tratarErroGenerico() {
+//        return "/login";
+//    }
+}
